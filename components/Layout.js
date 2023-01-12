8da49b2c9b405ebe6f466ba9a1d01e210f4a3e54
Shaun Latip <47066456+shaunlatip@users.noreply.github.com>
2023-01-11 23:47:08 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import NavigationBar from './NavigationBar'
-import Footer from './Footer'
+import Head from 'next/head'
 
 export default function Layout({ children }) {
   return (
     <>
+        <Head>
+            <title>Snazzy Portfolio</title>
+        </Head>
       <NavigationBar />
       <main>{children}</main>
-      <Footer/>
     </>
   )
 }
